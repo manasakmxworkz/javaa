f98ae008a2d4b321d9179c32a689c03d963f5744
manasakmxworkz <manasakm113@gmail.com>
2024-06-20 14:19:57 +0530

    Reliance.class

---
+++ Reliance.java
@@ -0,0 +1,11 @@
+class Reliance{
+
+		static String chocolates[]={"Dairy Milk","Munch","Ferror Rocher","Kit kat","Gone Mad"};
+		
+		
+		public static void main(String sweet[]){
+		
+		System.out.println("The available chocolate are:\n"+chocolates[0]+"\n"+chocolates[1]+"\n"+chocolates[2]+"\n"+chocolates[3]+"\n"+chocolates[4]);
+		
+		}
+		}
