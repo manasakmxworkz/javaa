3755043c884b1f0e371915aedb0e7d24a18a2754
manasakmxworkz <manasakm113@gmail.com>
2024-07-18 04:27:04 +0530

    Array and methods

---
+++ jun 26/AbhiBus.java
@@ -0,0 +1,13 @@
+class AbhiBus{
+
+	static String busNames[]={"Sea Bird","Vrl","Srs","Praveen","Rajahamsa"};
+	public static void main(String [] bus){
+	System.out.println("Main Started");
+	System.out.println("The available buses are: "+busNames.length);
+	for(String names :busNames){
+		System.out.println(names);
+	}
+	System.out.println("Main ended");
+		
+		}
+		}
---
+++ jun 26/Addition.java
@@ -0,0 +1,12 @@
+class Addition{
+
+	public static void main(String adds[]){
+	add (1,2);
+	add (3,4);
+	add (5,4);
+	}
+	public static void add(int num1,int num2){
+	System.out.println(num1+num2);
+	}
+	}
+	
